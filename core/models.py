9ca66bdf4c8f14028e2163f92b343c56825f151c
Hyun-Jun-Lee <mikey1000@naver.com>
2023-03-12 13:16:11 +0900

    <Add> : start project

---
+++ core/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Core(models.Model):
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
