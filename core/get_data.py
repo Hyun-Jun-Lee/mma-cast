5150ef65e603cfc50f005465b5b05966b776bc22
Hyun-Jun-Lee <mikey1000@naver.com>
2023-03-18 22:56:41 +0900

    <WIP> : Add height(cm),reach(cm),age

--- core/get_data.py
+++ core/get_data.py
@@ -1,5 +1,18 @@
 import pandas as pd
 
+"""
+    age = models.IntegerField()
+    name = models.TextField()
+    win = models.TextField()
+    lose = models.TextField()
+    height = models.TextField()
+    weight = models.TextField()
+    reach = models.TextField()
+    ko_wins = models.IntegerField()
+    ko_lose = models.IntegerField()
+"""
+
+
 col = [
     "date",
     "fight_url",
@@ -48,11 +61,25 @@ col_num = [i for i in range(len(col))]
 data = pd.read_csv("masterdataframe.csv", usecols=col_num)
 data["result"] = data["result"].replace({1: "승", 0: "패"})
 
+fighter_info = data.drop_duplicates("fighter")[["fighter", "age", "height", "reach"]]
+# inch -> sm
+fighter_info["height"] = round(fighter_info["height"] * 2.54, 1)
+fighter_info["reach"] = round(fighter_info["reach"] * 2.54, 1)
+# print(fighter_info.sample(5))
+
 
 def makes_win_lose_info(data):
     win_lose_info = data.groupby(["fighter", "result"])["result"].count().to_dict()
-    fighter_info = data["fighter"].drop_duplicates().to_frame()
-    fighter_names = fighter_info.values.flatten().tolist()
+    fighter_info = data.drop_duplicates("fighter")[
+        ["fighter", "age", "height", "reach"]
+    ]
+    # inch -> sm
+    fighter_info["height"] = round(fighter_info["height"] * 2.54, 1)
+    fighter_info["reach"] = round(fighter_info["reach"] * 2.54, 1)
+    # fighter_info = data["fighter"].drop_duplicates().to_frame()
+    fighter_names = (
+        data["fighter"].drop_duplicates().to_frame().values.flatten().tolist()
+    )
     win_temp = pd.DataFrame(index=range(len(fighter_names)), columns=["fighter", "win"])
     lose_temp = pd.DataFrame(
         index=range(len(fighter_names)), columns=["fighter", "lose"]
@@ -75,5 +102,9 @@ def makes_win_lose_info(data):
         else:
             lose_temp.loc[i] = [name, 0]
 
-    fighter_data = pd.merge(win_temp, lose_temp, how="left", on="fighter")
+    win_lose_data = pd.merge(win_temp, lose_temp, how="left", on="fighter")
+    fighter_data = pd.merge(fighter_info, win_lose_data, how="left", on="fighter")
     return fighter_data
+
+
+print(makes_win_lose_info(data))
