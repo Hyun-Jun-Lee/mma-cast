3ecbc01ce52815e77c5e242b9009bd1f78160a52
Hyun-Jun-Lee <mikey1000@naver.com>
2023-03-22 23:58:56 +0900

    <Fix> : Add scheduler

--- config/settings.py
+++ config/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_apscheduler",
+    "scheduler",
     "core",
     "fighters",
     "games",
