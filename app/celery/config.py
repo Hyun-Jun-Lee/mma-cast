2c7b30d508417fac75dd210122b549ac641cb0a2
Lee Hyun Jun <bhk0827@gmail.com>
2023-06-19 23:42:19 +0900

    <Feat> : Celery Routing

--- app/celery/config.py
+++ app/celery/config.py
@@ -6,5 +6,13 @@ from app.config import (
     CELERY_RESULT_BACKEND,
 )
 from celery import Celery
+from kombu import Queue
+
 
 app = Celery(CELERY_NAME, broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
+
+app.conf.task_default_queue = "default"
+app.conf.task_queues = (
+    Queue("crawling", routing_key="crawling.#"),
+    Queue("transform", routing_key="transform.#"),
+)
