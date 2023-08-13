cc30467daee357b607a8126fe5acf9512b48c866
Lee Hyun Jun <bhk0827@gmail.com>
2023-08-14 00:14:39 +0900

    <WIP> : tapology fighters crawling

--- app/airflow/dags/tapology_data_crawling.py
+++ app/airflow/dags/tapology_data_crawling.py
@@ -4,29 +4,47 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 from log import logger
 
-base_url = "https://www.tapology.com"
-search_url = base_url + "/search"
-req = requests.get(search_url).text
-time.sleep(1)
-html = BeautifulSoup(req, "html.parser")
+url_list = [
+    "https://www.tapology.com/search/mma-fighters-by-weight-class/Atomweight-105-pounds",
+    "https://www.tapology.com/search/mma-fighters-by-weight-class/Strawweight-115-pounds",
+    "https://www.tapology.com/search/mma-fighters-by-weight-class/Flyweight-125-pounds",
+    "https://www.tapology.com/search/mma-fighters-by-weight-class/Bantamweight-135-pounds",
+    "https://www.tapology.com/search/mma-fighters-by-weight-class/Featherweight-145-pounds",
+    "https://www.tapology.com/search/mma-fighters-by-weight-class/Lightweight-155-pounds",
+    "https://www.tapology.com/search/mma-fighters-by-weight-class/Welterweight-170-pounds",
+    "https://www.tapology.com/search/mma-fighters-by-weight-class/Middleweight-185-pounds",
+    "https://www.tapology.com/search/mma-fighters-by-weight-class/Light_Heavyweight-205-pounds",
+    "https://www.tapology.com/search/mma-fighters-by-weight-class/Heavyweight-265-pounds",
+    "https://www.tapology.com/search/mma-fighters-by-weight-class/Super_Heavyweight-over-265-pounds",
+]
 
-req = html.find("div", "siteSearchFightersByWeightClass")
-req = req.find_all("dd")
-for r in req:
-    weghit_class = r.text.strip()
-    href = r.find("a")["href"]
-    print(1212, weghit_class, href)
+semaphore = asyncio.Semaphore(10)
+
+
+url = "https://www.tapology.com/search/mma-fighters-by-weight-class/Super_Heavyweight-over-265-pounds"
+response = requests.get(url)
+html_content = response.text
+
+html = BeautifulSoup(html_content, "html.parser")
+res = html.find_all("table", "siteSearchResults")
 
 
 async def weight_classes():
-    base_url = "https://www.tapology.com"
-    search_url = base_url + "/search"
-    req = requests.get(search_url).text
-    time.sleep(1)
-    html = BeautifulSoup(req, "html.parser")
+    for url in url_list:
+        async with semaphore:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as res:
+                    res = await res.text()
+                    html = BeautifulSoup(res, "html.parser")
+                    coroutines = [
+                        fighters(url=a.get("href"))
+                        for table in res
+                        for a in table.find_all("a")
+                    ]
 
-    req = html.find("div", "siteSearchFightersByWeightClass")
-    req = req.find_all("dd")
-    for r in req:
-        weghit_class = r.text.strip()
-        print(1212, weghit_class)
+                    res = await asyncio.gather(*coroutines)
+    return res
+
+
+def fighters(url):
+    pass
