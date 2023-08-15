91561395e0e680cd871d892035d77bd55b7c5f9c
Lee Hyun Jun <bhk0827@gmail.com>
2023-08-15 19:22:27 +0900

    <Feat> : Add selenuim

--- app/airflow/dags/tapology_data_crawling.py
+++ app/airflow/dags/tapology_data_crawling.py
@@ -4,6 +4,28 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 from log import logger
 
+# 크롬 드라이버 기본 모듈
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+# 크롬 드라이버 자동 업데이트을 위한 모듈
+from webdriver_manager.chrome import ChromeDriverManager
+
+semaphore = asyncio.Semaphore(10)
+
+# 브라우저 꺼짐 방지 옵션
+chrome_options = Options()
+chrome_options.headless = True  # 브라우저 창이 띄우지 않게 설정
+chrome_options.add_experimental_option("detach", False)
+# 불필요한 에러 메시지 삭제
+chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
+# 크롬 드라이버 최신 버전 설정
+service = Service(executable_path=ChromeDriverManager().install())
+
+browser = webdriver.Chrome(service=service, options=chrome_options)
+
+
 url_list = [
     "https://www.tapology.com/search/mma-fighters-by-weight-class/Atomweight-105-pounds",
     "https://www.tapology.com/search/mma-fighters-by-weight-class/Strawweight-115-pounds",
@@ -18,24 +40,33 @@ url_list = [
     "https://www.tapology.com/search/mma-fighters-by-weight-class/Super_Heavyweight-over-265-pounds",
 ]
 
-semaphore = asyncio.Semaphore(10)
 
+url = "https://www.tapology.com/fightcenter/fighters/chan-sung-jung-the-korean-zombie"
 
-url = "https://www.tapology.com/search/mma-fighters-by-weight-class/Super_Heavyweight-over-265-pounds"
-response = requests.get(url)
-html_content = response.text
+browser.get(url)
+
+html_content = browser.page_source  # 모든 컨텐츠가 로드된 페이지의 HTML 소스 코드 가져오기
+
+browser.quit()  # 웹 드라이버 종료
 
 html = BeautifulSoup(html_content, "html.parser")
-res = html.find_all("table", "siteSearchResults")
+details_two_columns_div = html.find("div", id="stats")  # id를 사용하여 요소 찾기
+
+span_tags = details_two_columns_div.find_all("span")
+
+span_texts = [tag.text for tag in span_tags]
+
+print(span_texts)
 
 
 async def weight_classes():
     for url in url_list:
         async with semaphore:
             async with aiohttp.ClientSession() as session:
-                async with session.get(url) as res:
+                async with session.get(url, ssl=False) as res:
                     res = await res.text()
                     html = BeautifulSoup(res, "html.parser")
+                    res = html.find_all("table", "siteSearchResults")
                     coroutines = [
                         fighters(url=a.get("href"))
                         for table in res
@@ -46,5 +77,63 @@ async def weight_classes():
     return res
 
 
-def fighters(url):
-    pass
+sample = [
+    "정찬성, Jung Chan Sung",
+    "17-7-0 (Win-Loss-Draw)",
+    "\nThe Korean Zombie\n",
+    "1 Loss",
+    "36",
+    "1987.03.17",
+    "April 09, 2022",
+    "in",
+    "UFC",
+    "Featherweight",
+    "\n144.5 lbs\n",
+    "Fight Ready MMA",
+    "5'7\" (171cm)",
+    '72.0" (183cm)',
+    "$524,000 USD",
+    "\nPohang, South Korea\n",
+    "\nPohang, South Korea\n",
+    "\nEddie Cha\n",
+    "\nN/A\n",
+]
+
+
+async def fighters_detail(url):
+    model_dict = {}
+    browser.get(url)
+    html_content = browser.page_source  # 모든 컨텐츠가 로드된 페이지의 HTML 소스 코드 가져오기
+    browser.quit()  # 웹 드라이버 종료
+
+    html = BeautifulSoup(html_content, "html.parser")
+    details_two_columns_div = html.find("div", id="stats")  # id를 사용하여 요소 찾기
+
+    span_texts = [tag.text if "N/A" not in tag.text else None for tag in span_tags]
+
+    span_texts = [tag.text for tag in span_tags]
+    if span_texts:
+        name = (
+            span_texts[0].split(",")[1].replace(" ", "_")
+            if "," in span_texts[0]
+            else span_texts[0].replace(" ", "_")
+        )
+        model_dict["name"] = span_texts[0]
+        model_dict["pro_mma_record"] = span_texts[1]
+        model_dict["nickname"] = span_texts[2] if span_texts[2] else None
+        model_dict["current_streak"] = (
+            0 if span_texts[3] == "--" else inch_to_cm(span_texts[3])
+        )
+        model_dict["age"] = 0 if span_texts[4] == "--" else lbs_to_kg(span_texts[4])
+        model_dict["birth"] = None
+        model_dict["last_fight"] = 0 if span_texts[6] == "--" else span_texts[6]
+        model_dict["weight_class"] = 0 if span_texts[7] == "--" else span_texts[7]
+        model_dict["last_weigh_in"] = 0 if span_texts[8] == "--" else span_texts[8]
+        model_dict["affiliation"] = 0 if span_texts[9] == "--" else span_texts[9]
+        model_dict["height"] = 0 if span_texts[9] == "--" else span_texts[9]
+        model_dict["reach"] = 0 if span_texts[9] == "--" else span_texts[9]
+        model_dict["career_earnings"] = 0 if span_texts[9] == "--" else span_texts[9]
+        model_dict["born"] = 0 if span_texts[9] == "--" else span_texts[9]
+        model_dict["fighting_outof"] = 0 if span_texts[9] == "--" else span_texts[9]
+        model_dict["head_coach"] = 0 if span_texts[9] == "--" else span_texts[9]
+        model_dict["other_coanees"] = 0 if span_texts[9] == "--" else span_texts[9]
