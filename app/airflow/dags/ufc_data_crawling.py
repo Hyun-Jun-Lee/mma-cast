195929e9f3bbadb4376ac662b01b1ff3a433d5af
Lee Hyun Jun <bhk0827@gmail.com>
2024-01-16 11:47:49 +0900

    <Feat> : MongoDB

--- app/airflow/dags/ufc_data_crawling.py
+++ app/airflow/dags/ufc_data_crawling.py
@@ -2,8 +2,7 @@ import time, requests, re, asyncio, aiohttp
 from typing import List
 from bs4 import BeautifulSoup
 from datetime import datetime
-from app.db.models.data import DataFighter, DataMatch
-from app.db.session import get_ware_db
+from app.db.session import get_raw_db
 from log import logger
 
 semaphore = asyncio.Semaphore(10)  # Limit concurrent requests to 10
@@ -11,7 +10,11 @@ semaphore = asyncio.Semaphore(10)  # Limit concurrent requests to 10
 
 def inch_to_cm(data: str, is_reach=False):
     """
-    concert inch(ex.6'7") to cm
+    Inches를 센티미터로 변환합니다.
+
+    :param data: 변환할 데이터 문자열 (예: "6'0"").
+    :param is_reach: True일 경우, 리치 데이터로 간주하여 다르게 처리합니다.
+    :return: 변환된 센티미터 값.
     """
     if is_reach:
         reach = round((int(re.sub("[^0-9]", "", data)) * 2.54), 1)
@@ -26,11 +29,24 @@ def inch_to_cm(data: str, is_reach=False):
 
 
 def lbs_to_kg(data: str):
+    """
+    파운드를 킬로그램으로 변환합니다.
+
+    :param data: 변환할 데이터 문자열 (예: "170 lbs").
+    :return: 변환된 킬로그램 값.
+    """
     to_int = round((int(re.sub("[^0-9]", "", data)) / 2.205), 1)
     return to_int
 
 
 async def craw_fighter_info(session: aiohttp.ClientSession, url: str):
+    """
+    주어진 URL에서 파이터 정보를 비동기적으로 크롤링합니다.
+
+    :param session: aiohttp 클라이언트 세션.
+    :param url: 크롤링할 URL.
+    :return: 크롤링된 파이터 정보 리스트.
+    """
     async with session.get(url) as response:
         req = await response.text()
 
@@ -84,6 +100,13 @@ async def craw_fighter_info(session: aiohttp.ClientSession, url: str):
 
 
 async def craw_match(session: aiohttp.ClientSession, url: str) -> dict:
+    """
+    주어진 URL에서 매치 정보를 비동기적으로 크롤링합니다.
+
+    :param session: aiohttp 클라이언트 세션.
+    :param url: 크롤링할 URL.
+    :return: 크롤링된 매치 정보 리스트.
+    """
     async with semaphore:
         async with session.get(url) as response:
             res = await response.text()
@@ -100,10 +123,9 @@ async def craw_match(session: aiohttp.ClientSession, url: str) -> dict:
                 else None
             )
             match_date = (
-                datetime.strptime(match_info[0], "%B %d, %Y").date()
-                if match_info
-                else None
+                datetime.strptime(match_info[0], "%B %d, %Y") if match_info else None
             )
+
             match_loc = match_info[1] if match_info else None
             match_list = []
             for td in html.find_all(
@@ -138,6 +160,11 @@ async def craw_match(session: aiohttp.ClientSession, url: str) -> dict:
 
 
 async def craw_fighter():
+    """
+    모든 알파벳에 대해 UFC 통계 사이트에서 파이터 정보를 비동기적으로 크롤링합니다.
+
+    :return: 모든 알파벳에 대한 크롤링 결과 리스트.
+    """
     url_template = "http://www.ufcstats.com/statistics/fighters?char={}&page=all"
 
     async with aiohttp.ClientSession() as session:
@@ -153,6 +180,12 @@ async def craw_fighter():
 
 
 async def craw_game():
+    """
+    UFC 통계 사이트에서 완료된 모든 이벤트의 매치 정보를 비동기적으로 크롤링합니다.
+
+    :return: 크롤링된 모든 이벤트의 매치 정보 리스트.
+    """
+
     url = "http://www.ufcstats.com/statistics/events/completed?page=all"
 
     async with aiohttp.ClientSession() as session:
@@ -169,31 +202,11 @@ async def craw_game():
     return res
 
 
-def delete_all_fighters_data():
-    with get_ware_db() as db_session:
-        try:
-            db_session.query(DataFighter).delete()
-            db_session.commit()
-        except Exception as e:
-            logger.error(f"Error deleting fighters data: {e}")
-            db_session.rollback()
-        finally:
-            db_session.close()
-
-
-def delete_all_matches_data():
-    with get_ware_db() as db_session:
-        try:
-            db_session.query(DataMatch).delete()
-            db_session.commit()
-        except Exception as e:
-            logger.error(f"Error deleting matches data: {e}")
-            db_session.rollback()
-        finally:
-            db_session.close()
-
-
 def run_craw_game():
+    """
+    크롤링한 게임 데이터를 저장하기 위해 `craw_game` 함수를 실행
+    합니다. 실행 결과는 MongoDB에 저장됩니다.
+    """
     loop = asyncio.get_event_loop()
     match_data = loop.run_until_complete(craw_game())
     save_data_to_database(match_data=match_data)
@@ -205,32 +218,38 @@ def run_craw_fighter():
     save_data_to_database(fighters_data=fighters_data)
 
 
+def delete_all_data():
+    with get_raw_db() as db:
+        try:
+            db["matches"].delete_many({})
+            db["fighters"].delete_many({})
+        except Exception as e:
+            logger.error(f"Error deleting matches data: {e}")
+
+
 def save_data_to_database(
     fighters_data: List[dict] = None, match_data: List[dict] = None
 ):
+    delete_all_data()
+
     if fighters_data:
-        delete_all_fighters_data()
+        # delete_all_fighters_data() 함수도 이와 유사한 방식으로 수정 필요
         data_list = [
-            DataFighter(**{k: (None if v == "" else v) for k, v in fighter.items()})
+            {k: (None if v == "" else v) for k, v in fighter.items()}
             for fighter_list in fighters_data
             for fighter in fighter_list
         ]
+        with get_raw_db() as db:
+            db["fighters"].insert_many(data_list)
+            logger.warning(f"UFC fighters insert : {len(data_list)}")
     elif match_data:
-        delete_all_matches_data()
         data_list = [
-            DataMatch(**{k: (None if v == "" else v) for k, v in match.items()})
+            {k: (None if v == "" else v) for k, v in match.items()}
             for match_list in match_data
             for match in match_list
         ]
+        with get_raw_db() as db:
+            db["matches"].insert_many(data_list)
+            logger.warning(f"UFC matches insert : {len(data_list)}")
     else:
         return
-
-    with get_ware_db() as db_session:
-        try:
-            db_session.bulk_save_objects(data_list)
-            db_session.commit()
-        except Exception as e:
-            logger.error(f"Error bulk save data: {e}")
-            db_session.rollback()
-        finally:
-            db_session.close()
--- app/db/session.py
+++ app/db/session.py
@@ -1,16 +1,18 @@
 import os
 from typing import Generator
+from urllib.parse import quote_plus
 
 from pymongo import MongoClient
 from sqlalchemy import create_engine, inspect
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 from contextlib import contextmanager
-from app.db.models.data import DataFighter, DataMatch
 from app import config
 
+escaped_username = quote_plus(config.RAW_DB_USER)
+escaped_password = quote_plus(config.RAW_DB_PASSWORD)
 
-SQLALCHEMY_DATABASE_URL = f"mysql://{config.DATABASE_USER}:{config.DATABASE_PASSWORD}@{config.DATABASE_HOST}:{config.DATABASE_PORT}/{config.DATABASE_NAME}?charset=utf8"
-MONGODB_DATABASE_URL = f"mongodb+srv://{config.RAW_DB_USER}:{config.RAW_DB_PASSWORD}@{config.CLUSTER_NAME}.7bddfbe.mongodb.net/{config.RAW_DB_NAME}?retryWrites=true&w=majority"
+SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{config.DATABASE_USER}:{config.DATABASE_PASSWORD}@{config.DATABASE_HOST}:{config.DATABASE_PORT}/{config.DATABASE_NAME}"
+MONGODB_DATABASE_URL = f"mongodb+srv://{escaped_username}:{escaped_password}@{config.CLUSTER_NAME}.7bddfbe.mongodb.net/{config.RAW_DB_NAME}?retryWrites=true&w=majority"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
@@ -20,7 +22,9 @@ engine = create_engine(
     encoding="utf8",
     convert_unicode=True,
 )
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = scoped_session(
+    sessionmaker(autocommit=False, autoflush=False, bind=engine)
+)
 
 
 @contextmanager
@@ -29,7 +33,7 @@ def get_raw_db():
     호출되면 MongoDB 연결하고 작업 완료되면 close
     """
     client = MongoClient(MONGODB_DATABASE_URL)
-    db = client.get_default_database()  # 기본 데이터베이스 사용, 또는 명시적으로 데이터베이스 이름 지정 가능
+    db = client.get_default_database()
     try:
         yield db
     finally:
