e8324c92c40e91b511648ad87f75437a1cc41d9f
Lee Hyun Jun <bhk0827@gmail.com>
2024-02-02 02:56:28 +0900

    <WIP> : sperate utils

--- app/airflow/dags/ufc_data_crawling.py
+++ app/airflow/dags/ufc_data_crawling.py
@@ -1,58 +1,10 @@
-import time, requests, re, asyncio, aiohttp
-from typing import List
+import asyncio, aiohttp
 from bs4 import BeautifulSoup
 from datetime import datetime
-from pymongo import errors
 
-from app.db.session import get_raw_db
-from log import logger
+from utils import inch_to_cm, lbs_to_kg, save_data
 
 semaphore = asyncio.Semaphore(10)  # Limit concurrent requests to 10
-
-
-def check_connection():
-    try:
-        # get_raw_db 컨텍스트 매니저를 사용하여 MongoDB 연결 시도
-        with get_raw_db() as db:
-            # 서버 상태 확인
-            db.command("ping")
-            print("MongoDB connection: Success")
-    except errors.ServerSelectionTimeoutError as err:
-        # 연결 실패시 에러 메시지
-        print("MongoDB connection: Failed", err)
-        logger.error(f"Error when Connection db : {err}")
-        raise
-
-
-def inch_to_cm(data: str, is_reach=False):
-    """
-    Inches를 센티미터로 변환합니다.
-
-    :param data: 변환할 데이터 문자열 (예: "6'0"").
-    :param is_reach: True일 경우, 리치 데이터로 간주하여 다르게 처리합니다.
-    :return: 변환된 센티미터 값.
-    """
-    if is_reach:
-        reach = round((int(re.sub("[^0-9]", "", data)) * 2.54), 1)
-        return reach
-    H_feet = data.split(" ")[0]
-    H_inch = data.split(" ")[-1]
-
-    H_feet_e = int(re.sub("[^0-9]", "", H_feet)) * 12
-    H_inch_e = int(re.sub("[^0-9]", "", H_inch))
-    to_cm = round(((H_feet_e + H_inch_e) * 2.54), 1)
-    return to_cm
-
-
-def lbs_to_kg(data: str):
-    """
-    파운드를 킬로그램으로 변환합니다.
-
-    :param data: 변환할 데이터 문자열 (예: "170 lbs").
-    :return: 변환된 킬로그램 값.
-    """
-    to_int = round((int(re.sub("[^0-9]", "", data)) / 2.205), 1)
-    return to_int
 
 
 async def fetch_fighter_info_async(
@@ -234,52 +186,10 @@ def execute_match_info_fetching():
     """
     loop = asyncio.get_event_loop()
     match_data = loop.run_until_complete(fetch_all_matches_info_async())
-    save_data_to_database(match_data=match_data)
+    save_data(match_data=match_data)
 
 
 def execute_fighter_info_fetching():
     loop = asyncio.get_event_loop()
     fighters_data = loop.run_until_complete(fetch_all_fighters_info_async())
-    save_data_to_database(fighters_data=fighters_data)
-
-
-def save_data_to_database(
-    fighters_data: List[dict] = None, match_data: List[dict] = None
-):
-    if fighters_data:
-        data_list = [
-            {k: (None if v == "" else v) for k, v in fighter.items()}
-            for fighter_list in fighters_data
-            for fighter in fighter_list
-        ]
-        with get_raw_db() as db:
-            try:
-                db["fighters"].delete_many({})
-            except Exception as e:
-                logger.error(f"Error deleting fighters data: {e}")
-
-            try:
-                db["fighters"].insert_many(data_list)
-                logger.warning(f"UFC fighters insert : {len(data_list)}")
-            except Exception as e:
-                logger.error(f"Error save fighters data: {e}")
-
-    elif match_data:
-        data_list = [
-            {k: (None if v == "" else v) for k, v in match.items()}
-            for match_list in match_data
-            for match in match_list
-        ]
-        with get_raw_db() as db:
-            try:
-                db["fighters"].delete_many({})
-            except Exception as e:
-                logger.error(f"Error deleting match data: {e}")
-
-            try:
-                db["matches"].insert_many(data_list)
-                logger.warning(f"UFC matches insert : {len(data_list)}")
-            except Exception as e:
-                logger.error(f"Error save match data: {e}")
-    else:
-        return
+    save_data(fighters_data=fighters_data)
---
+++ app/airflow/dags/utils.py
@@ -0,0 +1,91 @@
+import re
+from typing import List
+
+from app.db.session import get_raw_db
+from pymongo import errors
+from log import logger
+
+
+def inch_to_cm(data: str, is_reach=False):
+    """
+    Inches를 센티미터로 변환합니다.
+
+    :param data: 변환할 데이터 문자열 (예: "6'0"").
+    :param is_reach: True일 경우, 리치 데이터로 간주하여 다르게 처리합니다.
+    :return: 변환된 센티미터 값.
+    """
+    if is_reach:
+        reach = round((int(re.sub("[^0-9]", "", data)) * 2.54), 1)
+        return reach
+    H_feet = data.split(" ")[0]
+    H_inch = data.split(" ")[-1]
+
+    H_feet_e = int(re.sub("[^0-9]", "", H_feet)) * 12
+    H_inch_e = int(re.sub("[^0-9]", "", H_inch))
+    to_cm = round(((H_feet_e + H_inch_e) * 2.54), 1)
+    return to_cm
+
+
+def lbs_to_kg(data: str):
+    """
+    파운드를 킬로그램으로 변환합니다.
+
+    :param data: 변환할 데이터 문자열 (예: "170 lbs").
+    :return: 변환된 킬로그램 값.
+    """
+    to_int = round((int(re.sub("[^0-9]", "", data)) / 2.205), 1)
+    return to_int
+
+
+def check_connection():
+    try:
+        # get_raw_db 컨텍스트 매니저를 사용하여 MongoDB 연결 시도
+        with get_raw_db() as db:
+            # 서버 상태 확인
+            db.command("ping")
+            print("MongoDB connection: Success")
+    except errors.ServerSelectionTimeoutError as err:
+        # 연결 실패시 에러 메시지
+        print("MongoDB connection: Failed", err)
+        logger.error(f"Error when Connection db : {err}")
+        raise
+
+
+def save_data(fighters_data: List[dict] = None, match_data: List[dict] = None):
+    if fighters_data:
+        data_list = [
+            {k: (None if v == "" else v) for k, v in fighter.items()}
+            for fighter_list in fighters_data
+            for fighter in fighter_list
+        ]
+        with get_raw_db() as db:
+            try:
+                db["fighters"].delete_many({})
+            except Exception as e:
+                logger.error(f"Error deleting fighters data: {e}")
+
+            try:
+                db["fighters"].insert_many(data_list)
+                logger.warning(f"UFC fighters insert : {len(data_list)}")
+            except Exception as e:
+                logger.error(f"Error save fighters data: {e}")
+
+    elif match_data:
+        data_list = [
+            {k: (None if v == "" else v) for k, v in match.items()}
+            for match_list in match_data
+            for match in match_list
+        ]
+        with get_raw_db() as db:
+            try:
+                db["fighters"].delete_many({})
+            except Exception as e:
+                logger.error(f"Error deleting match data: {e}")
+
+            try:
+                db["matches"].insert_many(data_list)
+                logger.warning(f"UFC matches insert : {len(data_list)}")
+            except Exception as e:
+                logger.error(f"Error save match data: {e}")
+    else:
+        return
