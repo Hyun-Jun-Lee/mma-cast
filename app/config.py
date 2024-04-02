bd1874069dc4658bc832cf2a29f03083d505d5b9
Lee Hyun Jun <bhk0827@gmail.com>
2024-04-02 17:54:29 +0900

    <Fix> : clean env

--- app/config.py
+++ app/config.py
@@ -11,16 +11,7 @@ DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
 DATABASE_NAME = os.environ.get("DATABASE_NAME")
 
 # mongodb
-RAW_DB_USER = os.environ.get("RAW_DB_USER")
-RAW_DB_PASSWORD = os.environ.get("RAW_DB_PASSWORD")
-RAW_DB_NAME = os.environ.get("RAW_DB_NAME")
+MONGO_INITDB_ROOT_USERNAME = os.environ.get("MONGO_INITDB_ROOT_USERNAME")
+MONGO_INITDB_ROOT_PASSWORD = os.environ.get("MONGO_INITDB_ROOT_PASSWORD")
+MONGODB_NAME = os.environ.get("MONGODB_NAME")
 CLUSTER_NAME = os.environ.get("CLUSTER_NAME")
-
-# celery
-CELERY_NAME = os.environ.get("CELERY_NAME")
-CELERY_POOL = os.environ.get("CELERY_POOL")
-CELERY_CONCURRENCY = os.environ.get("CELERY_CONCURRENCY")
-RABBITMQ_USER = os.environ.get("RABBITMQ_USER")
-RABBITMQ_PW = os.environ.get("RABBITMQ_PW")
-CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL")
-CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND")
