8f5eacc33940af4cc04ddbcea23382efab0147f2
Lee Hyun Jun <bhk0827@gmail.com>
2024-04-15 15:45:07 +0900

    <Feat> : add sql init

--- app/init_sql/sql_init.sql
+++ app/init_sql/sql_init.sql
@@ -1,2 +1,10 @@
+-- 사용자 생성
+CREATE USER mma_cast WITH ENCRYPTED PASSWORD 'mma_cast';
+
+-- 데이터베이스 생성
 CREATE DATABASE airflow;
 CREATE DATABASE mma_cast;
+
+-- 데이터베이스에 대한 권한 부여
+GRANT ALL PRIVILEGES ON DATABASE airflow TO mma_cast;
+GRANT ALL PRIVILEGES ON DATABASE mma_cast TO mma_cast;
