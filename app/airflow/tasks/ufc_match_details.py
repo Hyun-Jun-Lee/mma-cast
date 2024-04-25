fcec4ae371571fb2be0a9a1a7fb9fa8fbd179e38
Lee Hyun Jun <bhk0827@gmail.com>
2024-04-25 16:15:39 +0900

    <WIP> : round detail

--- app/airflow/tasks/ufc_match_details.py
+++ app/airflow/tasks/ufc_match_details.py
@@ -6,36 +6,64 @@ from .utils import save_data, get_element_safe
 from dags.log import logger
 
 
+async def extract_stats(cell_texts):
+    fighters_names = cell_texts[0].split("\n\n\n")
+    sig_strike_stats = cell_texts[2].split()
+    takedown_stats = cell_texts[5].split()
+    sub_attack_stats = cell_texts[7].split()
+    control_time_stats = cell_texts[8].split()
+
+    red_fighter = {
+        "name": fighters_names[0],
+        "total_strike": sig_strike_stats[2],
+        "hit_strike": sig_strike_stats[0],
+        "total_takedown": takedown_stats[2],
+        "takedown": takedown_stats[0],
+        "sub_attacks": sub_attack_stats[0],
+        "control_time": control_time_stats[0],
+    }
+    blue_fighter = {
+        "name": fighters_names[1],
+        "total_strike": sig_strike_stats[3],
+        "hit_strike": sig_strike_stats[1],
+        "total_takedown": takedown_stats[3],
+        "takedown": takedown_stats[1],
+        "sub_attacks": sub_attack_stats[1],
+        "control_time": control_time_stats[1],
+    }
+
+    return red_fighter, blue_fighter
+
+
+async def extrat_total_round_fight_stat(tbody):
+    total_round_stat = []
+    round_info = 0
+
+    for tr in tbody.find_all("tr"):
+        each_round_stat = {}
+        round_info += 1
+        each_round_stat["round"] = round_info
+        cell_texts = [td.text.strip() for td in tr.find_all("td")]
+
+        red_fighter, blue_fighter = await extract_stats(cell_texts)
+
+        each_round_stat["red_fighter"] = red_fighter
+        each_round_stat["blue_fighter"] = blue_fighter
+        total_round_stat.append(each_round_stat)
+
+    return total_round_stat
+
+
 async def extrat_total_fight_stat(tbody):
+    total_stat = []
     for tr in tbody.find_all("tr"):
         cell_texts = [td.text.strip() for td in tr.find_all("td")]
 
-        fighters_names = cell_texts[0].split("\n\n\n")
-        sig_strike_stats = cell_texts[2].split()
-        takedown_stats = cell_texts[5].split()
-        sub_attack_stats = cell_texts[7].split()
-        control_time_stats = cell_texts[8].split()
+        red_fighter, blue_fighter = await extract_stats(cell_texts)
 
-        red_fighter = {
-            "name": fighters_names[0],
-            "total_strike": sig_strike_stats[2],
-            "hit_strike": sig_strike_stats[0],
-            "total_takedown": takedown_stats[2],
-            "takedown": takedown_stats[0],
-            "sub_attacks": sub_attack_stats[0],
-            "control_time": control_time_stats[0],
-        }
-        blue_fighter = {
-            "name": fighters_names[1],
-            "total_strike": sig_strike_stats[3],
-            "hit_strike": sig_strike_stats[1],
-            "total_takedown": takedown_stats[3],
-            "takedown": takedown_stats[1],
-            "sub_attacks": sub_attack_stats[1],
-            "control_time": control_time_stats[1],
-        }
-
-    return red_fighter, blue_fighter
+    total_stat.append(red_fighter)
+    total_stat.append(blue_fighter)
+    return total_stat
 
 
 async def fetch_fight_links(session, url, semaphore, all_fight_links):
@@ -85,36 +113,28 @@ async def fetch_all_fight(session, semaphore):
 
 async def fetch_fight_stat(session, semaphore, fight_links):
     async with semaphore:
-        for url in fight_links:
-            # url = "http://www.ufcstats.com/fight-details/894c44c3d04aaf6f"
-            async with session.get(url) as response:
-                res = await response.text()
-                html = BeautifulSoup(res, "html.parser")
+        # for url in fight_links:
+        url = "http://www.ufcstats.com/fight-details/894c44c3d04aaf6f"
+        fighte_info = {}
+        async with session.get(url) as response:
+            res = await response.text()
+            html = BeautifulSoup(res, "html.parser")
 
-                main_event_title = html.find("h2").get_text(strip=True)
+            main_event_title = html.find("h2").get_text(strip=True)
+            fighte_info["main_event"] = main_event_title
 
-                all_tables = html.find_all("table")
-                total_tables = all_tables[0].find("tbody")
-                total_round_tables = all_tables[1].find("tbody")
-                total_detail_strike_tables = all_tables[2].find("tbody")
-                total_detail_strike_round_tables = all_tables[3].find("tbody")
+            all_tables = html.find_all("table")
+            total_tables = all_tables[0].find("tbody")
+            total_round_tables = all_tables[1].find("tbody")
+            total_detail_strike_tables = all_tables[2].find("tbody")
+            total_detail_strike_round_tables = all_tables[3].find("tbody")
 
-                # TODO : 1,2,3 table
-                # red_fighter_total, blue_fighter_total = await extrat_total_fight_stat(total_tables)
+            total_detail = await extrat_total_fight_stat(total_tables)
 
-            # headers = []
-            # for th in html.find_all("th"):
-            #     headers.append(th.text.strip())
-            # print(headers)
+            round_detail = await extrat_total_round_fight_stat(total_round_tables)
 
-            # for tr in html.find_all("tr"):
-            #     tds = tr.find_all("td")
-            #     if not tds:
-            #         continue
-            #     row_data = []
-            #     for td in tds:
-            #         row_data.append(td.get_text(strip=True))
-            #     print(row_data)
+            fighte_info["total"] = total_detail
+            fighte_info["rounds"] = round_detail
 
 
 async def main():
